04e8c2bf3eefab7e954b046142e859a1453961cc
VivineyD1az <141343894+VivineyD1az@users.noreply.github.com>
2025-04-06 03:36:08 -0500

    ✨Introduce new features.

--- login-mobile/src/LoginForm.jsx
+++ login-mobile/src/LoginForm.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginForm.css";
-import Icon from "./assets/Icon.svg"; 
+import Icon from "./assets/Icon.svg";
 
 function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleLogin = () => {
+    console.log("Username:", username);
+    console.log("Password:", password);
+    console.log("Remember Me:", rememberMe ? "Sí" : "No");
+
+    // Limpiar campos
+    setUsername("");
+    setPassword("");
+    setRememberMe(false);
+  };
+
   return (
     <div className="login-container">
       <div className="circle circle-top-left"></div>
@@ -10,19 +25,42 @@ function LoginForm() {
 
       <div className="login-box">
         <div className="user-icon">
-          <img src={Icon} alt="user icon" /> 
+          <img src={Icon} alt="user icon" />
         </div>
 
-        <input type="text" placeholder="Username" className="input" />
-        <input type="password" placeholder="Password" className="input" />
+        <input
+          type="text"
+          placeholder="Username"
+          className="input"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <div className="remember-container">
-          <input type="checkbox" id="remember" />
+          <input
+            type="checkbox"
+            id="remember"
+            checked={rememberMe}
+            onChange={(e) => setRememberMe(e.target.checked)}
+          />
           <label htmlFor="remember">Remember me</label>
         </div>
 
-        <button className="login-button">Sign in</button>
-        <a href="#" className="forgot-password">Forgot password?</a>
+        <button className="login-button" onClick={handleLogin}>
+          Sign in
+        </button>
+
+        <a href="#" className="forgot-password">
+          Forgot password?
+        </a>
       </div>
     </div>
   );
